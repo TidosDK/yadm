55f8e5192fdc668e90584c86f10f672b3048a0f1
TidosDK <28749155+TidosDK@users.noreply.github.com>
2023-11-05 18:43:34 +0100

    Added more files

---
+++ .config/fish/functions/_tide_item_kubectl.fish
@@ -0,0 +1,6 @@
+function _tide_item_kubectl
+    if command -v kubectl &> /dev/null
+        kubectl config view --minify --output 'jsonpath={.current-context}/{..namespace}' 2>/dev/null | read -l context &&
+        _tide_print_item kubectl $tide_kubectl_icon' ' (string replace -r '/(|default)$' '' $context)
+    end
+end
